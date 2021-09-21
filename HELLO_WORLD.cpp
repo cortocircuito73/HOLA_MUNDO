4ed945ba2a43684e94401f8165afd032c1049549
cortocircuito73 <90864761+cortocircuito73@users.noreply.github.com>
2021-09-21 17:00:23 -0500

    HOLA MUNDO C++

---
+++ HELLO_WORLD.cpp
@@ -0,0 +1,24 @@
+//HOLA MUNDO
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string titulo="HOLA MUNDO";
+	int pos=titulo.length();
+	
+	pos+=10;
+	cout<<endl;
+	
+	for(int i=0; i<=pos; i++){
+		cout<<" ";
+		if(i==7){
+			  						cout<<titulo;
+			i=pos;
+		}
+		cout<<" ";
+	}
+	return 0;
+}
+
